91c4ddc0a30c7f9c27bdb01349945b7ef3cf4fe5
Taylor Hill <54647081+TaylorHill001@users.noreply.github.com>
2019-12-10 23:08:47 -0800

    Additional Files

---
+++ SumOfDigits/SumOfDigits/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * C# Program to Get a Number and Display the Sum of the Digits 
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //First establish your var's
+            int num, sum = 0, r;
+            Console.WriteLine("Enter a Number : ");
+            num = int.Parse(Console.ReadLine());
+            //Here we create a while loop and while it isnt set to zero
+            //the loop will evaluate the sum of digits
+            while (num != 0)
+            {
+                r = num % 10;
+                num = num / 10;
+                sum = sum + r;
+            }
+            Console.WriteLine("Sum of Digits of the Number : " + sum);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ SwapTwoNumber/SwapTwoNumber/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * C# Program to Swap two Numbers
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //First we assign three var's. two are the numbers we will
+            //be switching and the other will be a placeholder to hold
+            //one of the numbers while asigning one number to the other
+            int num1, num2, temp;
+            //assign the first two numbers
+            Console.Write("\nEnter the First Number : ");
+            num1 = int.Parse(Console.ReadLine());
+            Console.Write("\nEnter the Second Number : ");
+            num2 = int.Parse(Console.ReadLine());
+            //this is where temp holds the first number, then the two
+            //other numbers will be switched
+            temp = num1;
+            num1 = num2;
+            num2 = temp;
+            //here we write out the numbers to show they are swapped
+            Console.Write("\nAfter Swapping : ");
+            Console.Write("\nFirst Number : " + num1);
+            Console.Write("\nSecond Number : " + num2);
+            Console.Read();
+        }
+    }
+}
