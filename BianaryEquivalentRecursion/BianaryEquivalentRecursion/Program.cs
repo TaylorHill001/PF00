84debb91c2c1e835b042836612df75774c2dabfc
Taylor Hill <54647081+TaylorHill001@users.noreply.github.com>
2019-12-10 23:06:15 -0800

    Adding First Two Projects

---
+++ BianaryEquivalentRecursion/BianaryEquivalentRecursion/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * C# Program to Print Binary Equivalent of an Integer using Recursion
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int num;
+        prog pg = new prog();
+        Console.WriteLine("Enter a decimal number: ");
+        num = int.Parse(Console.ReadLine());
+        Console.WriteLine("The binary equivalent of num is :");
+        pg.binaryconversion(num);
+        Console.ReadLine();
+    }
+}
+public class prog
+{
+    public int binaryconversion(int num)
+    {
+        //If else condition statement is used to check the value of ‘num’ variable is equal to 0.
+        //If the condition is true then execute the statement and return the value of 0.
+        //if the condition is false then execute the else statement. Compute the modulus
+        //of the value of ‘num’ variable by 2. Add the value of ‘result’ variable to 10.
+        //Multiply the value of ‘result’ variable with the value of binary_conversion() function.
+        int bin;
+        if (num != 0)
+        {
+            bin = (num % 2) + 10 * binaryconversion(num / 2);
+            Console.Write(bin);
+            return 0;
+        }
+        else
+        {
+            return 0;
+        }
+
+    }
+}
---
+++ EvenOdd/EvenOdd/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * C# Program to Check whether the Entered Number is Even or Odd
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace check1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //First we establish a var, and promp the user to enter an input
+            int i;
+            Console.Write("Enter a Number : ");
+            //Here we take the input, which is entered as a string, and parse it, then asign it to our var
+            i = int.Parse(Console.ReadLine());
+            //We then have an if else statement where we determin if the var is
+            //divisible by 2, where the result determins if it is an even or odd num
+            if (i % 2 == 0)
+            {
+                Console.Write("Entered Number is an Even Number");
+                Console.Read();
+            }
+            else
+            {
+                Console.Write("Entered Number is an Odd Number");
+                Console.Read();
+            }
+        }
+    }
+}
